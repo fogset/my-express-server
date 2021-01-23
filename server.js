b03be628281dee751711a98ecf6d00a9bcf8fc50
fogset <zhangth1990@hotmail.com>
2021-01-22 18:13:19 -0800

    First Server finished

--- server.js
+++ server.js
@@ -1,3 +1,23 @@
 
 const express = require("express");
 const app = express();
+
+app.get("/", function(request, response){
+  response.send("<h1>Hello, world!");
+});
+
+app.get("/Contact", function(request, response){
+  response.send("Contact me at: angla@gmail.com");
+});
+
+app.get("/about", function(request, response){
+  response.send("My name is tian I own this website");
+});
+
+app.get("/hobbies", function(request, response){
+  response.send("play computer games");
+});
+
+app.listen(3000, function(){
+  console.log("Server started on port 3000");
+});
